645b96f7d74c8b0bc65a6c42a9b26cdedea8f16b
Kyoung Yeob <kkyub1313@gmail.com>
2024-11-25 17:02:03 +0900

    feat(nextjs): #3.4 Parallel Requests(1)

--- learn-nextjs14/app/(home)/page.tsx
+++ learn-nextjs14/app/(home)/page.tsx
@@ -1,16 +1,18 @@
 // "use client"
 
+import Link from "next/link";
+
 // import { useEffect, useState } from "react";
 
 export const metadata = {
     title: "Home",
 };
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 // 기존 Client 측 컴포넌트에서 Server 측 컴포넌트로 변경
 async function getMovies() {
-    await new Promise((resolve, reject) => setTimeout(resolve, 5000));
-    const response = await fetch(URL);
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+    const response = await fetch(API_URL);
     const json = await response.json();
     return json;
 }
@@ -30,7 +32,7 @@ export default async function HomePage() {
     const movies = await getMovies();
     return (
         <div>
-            {JSON.stringify(movies)}
+            {movies.map(movie => <li key={movie.id}><Link href={`/movies/${movie.id}`}>{movie.title}</Link></li>)}
         </div>
     )
 }
--- learn-nextjs14/app/(movies)/movies/[id]/page.tsx
+++ learn-nextjs14/app/(movies)/movies/[id]/page.tsx
@@ -1,9 +1,21 @@
+import { API_URL } from "../../../(home)/page";
+
+async function getMovies(id: string){
+    const response = await fetch(`${API_URL}/${id}`);
+    return response.json();
+}
+
 export default async function MovieDetail({
-    params,
+    params : {id},
 }: {
     params: { id: string };
 }) {
-    const { id } =  await params;
-    console.log(params);
-    return <h1>Movie {id}</h1>;
+    const movie = await getMovies(id);
+    return <h1>{movie.title}</h1>;
 }
+
+// export default async function MovieDetail(context: { params: Promise<{ id: string }> }) {
+//     const { id } = await context.params; // 비동기적으로 params를 처리
+//     const movie = await getMovies(id);
+//     return <h1>{movie.title}</h1>;
+//   }
