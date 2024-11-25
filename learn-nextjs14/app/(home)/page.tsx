b30017a56cd7a4120c40210af2bad7213c7727a9
Kyoung Yeob <kkyub1313@gmail.com>
2024-11-25 15:48:11 +0900

    feat(nextjs): #3.2~ #3.3 Server Side / Loading Component

--- learn-nextjs14/app/(home)/page.tsx
+++ learn-nextjs14/app/(home)/page.tsx
@@ -1,23 +1,36 @@
-"use client"
+// "use client"
 
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-export default function Page() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async () => {
-        const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
-        const json = await response.json();
-        setMovies(json);
-        setIsLoading(false);
-    };
-    useEffect(() => {
-        getMovies();
-    }, []);
+export const metadata = {
+    title: "Home",
+};
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
+// 기존 Client 측 컴포넌트에서 Server 측 컴포넌트로 변경
+async function getMovies() {
+    await new Promise((resolve, reject) => setTimeout(resolve, 5000));
+    const response = await fetch(URL);
+    const json = await response.json();
+    return json;
+}
+export default async function HomePage() {
+    // const [isLoading, setIsLoading] = useState(true);
+    // const [movies, setMovies] = useState([]);
+    // const getMovies = async () => {
+    //     const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
+    //     const json = await response.json();
+    //     setMovies(json);
+    //     setIsLoading(false);
+    // };
+    // useEffect(() => {
+    //     getMovies();
+    // }, []);
+
+    const movies = await getMovies();
     return (
         <div>
-            {isLoading ? "Loading... please wait..." : JSON.stringify(movies)}
+            {JSON.stringify(movies)}
         </div>
     )
 }
